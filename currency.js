764e28a46158c295076047c6b85621a8a64d7a44
waqasii <waqas199888@gmail.com>
2023-02-01 16:45:00 +0100

    version 1 completed, excahgne button and swap button work perfectly!

--- currency.js
+++ currency.js
@@ -1,12 +1,11 @@
-// window.onload = runFunctionsOnLoad;
+window.onload = runFunctionsOnLoad;
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", "Mps2Uft1uEwEEsEsYFx61uNosm9tvjHG");
 
 function get_currencies() {
 
     console.log('we are in js file')
-
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "Mps2Uft1uEwEEsEsYFx61uNosm9tvjHG");
-
     var requestOptions = {
         method: 'GET',
         redirect: 'follow',
@@ -30,7 +29,6 @@ function get_currencies() {
 
 
 }
-
 
 function addOptionsToSelect(selectElements) {
 
@@ -65,6 +63,7 @@ function addOptionsToSelect(selectElements) {
 }
 
 function runFunctionsOnLoad() {
+
     // add option to select of base currency
     const base_selectElement = document.querySelector(".base-currency-select");
     const quote_selectElement = document.querySelector(".quote-currency-select");
@@ -74,15 +73,60 @@ function runFunctionsOnLoad() {
 
 }
 
-
 function submitExchange() {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+
+    console.log('We are in Submit function!!!');
+    base_currency = document.getElementById("base_currency").value;
+    quote_currency = document.getElementById("quote_currency");
+
+    base_currency_option = document.getElementById("base_currency_option").value;
+    quote_currency_option = document.getElementById("quote_currency_option").value;
+
+    console.log(base_currency_option, base_currency);
+    console.log(quote_currency_option)
+    // console.log(quote_currency_option, quote_currency);
+
+    fetch("https://api.apilayer.com/fixer/convert?to=" + quote_currency_option + "&from=" + base_currency_option + "&amount=" + base_currency, requestOptions)
+        .then(response => { return response.json() })
+        .then(result => {
+            quote_currency.disabled = false;
+            quote_currency.value = result.result
+            quote_currency.disabled = true;
+            console.log(result.result);
+        })
+        .catch(error => { alert('Error in communication with an API') });
+
+}
+
+function swapCurrency() {
+    base_currency = document.getElementById("base_currency");
+    quote_currency = document.getElementById("quote_currency");
+
+    base_currency_option = document.getElementById("base_currency_option");
+    quote_currency_option = document.getElementById("quote_currency_option");
+
+    let temp_currency = base_currency.value
+    base_currency.value = quote_currency.value
+    quote_currency.disabled = false;
+    quote_currency.value = temp_currency
+    quote_currency.disabled = true;
+
+    temp_currency = base_currency_option.value
+    base_currency_option.value = quote_currency_option.value
+    quote_currency_option.value = temp_currency
 
 
 }
 
+
 // Test change
 // API KEY
-// Mps2Uft1uEwEEsEsYFx61uNosm9tvjHG
-
+// Mps2Uft1uEwEEsEsYFx61uNosm9tvjHG   WAQAS
+// XTqi6KumotxKhVwLbxm2ue37edozpXdl  USMAN
 // Documentation
 // https://exchangeratesapi.io/documentation/
